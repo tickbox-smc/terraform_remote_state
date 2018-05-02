ef6830b7d592c96054c756e5100128b51ef2ba27
Falckster <chris.falck@tickboxconsulting.com>
2018-05-02 15:11:25 +0100

    add files

---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+# terraform state file setup
+# create an S3 bucket to store the state file in
+resource "aws_s3_bucket" "terraform-state-storage-s3" {
+  bucket = "${var.bucket_name}"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  tags {
+      Name = "S3 Remote Terraform State Store"
+  } 
+}
+
+# create a dynamodb table for locking the state file
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "${var.dynamodb_statelock_name}"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+ 
+  tags {
+    Name = "DynamoDB Terraform State Lock Table"
+  }
+}
+
+# This is being created to force a state change into S3
+# Can be commented out to force change to prove versioning is taking place
+#resource "aws_instance" "example" {
+#  ami           = "ami-c12dcda6"
+#  instance_type = "t2.micro"
+#}
---
+++ vars.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "The region for deployment"
+  default     = "eu-west-2"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket. Must be globally unique!"
+  default     = "tickbox-smc-s3-statestore"
+}
+
+variable "dynamodb_statelock_name" {
+  description = "The name of the DynamoDB table used to lock the state file."
+  default     = "tickbox-smc-dynamodb-statelock"
+}
